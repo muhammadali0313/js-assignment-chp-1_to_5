67a7efec855b3682a4b17fe5fbd41c91b3374c81
Muhammad Ali <153639747+muhammadali0313@users.noreply.github.com>
2024-03-08 23:54:04 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+alert("Assalam-o-Alaikum");
+alert("Error! Please Enter a Valid Password");
+alert("Welcom to Js Land... \n Happy Coding!");
+alert("Welcome to JS Land...");
+alert("Happy Coding!");
+alert("Hello... I can run JS through my Web's Browser Console");
+var hello = "Hello... I can run JS through my Web's Browser Console";
+console.log(hello);
